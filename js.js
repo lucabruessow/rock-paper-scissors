6f76d06cb7520f95b5d633ebd0637ada8bad6c10
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-07-24 20:03:04 +0200

    refactor: rename variable for more clarity

--- js.js
+++ js.js
@@ -1,12 +1,12 @@
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3 + 1);
-    if (choice == 1) {
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    if (computerChoice == 1) {
         console.log("Rock")
     }
-    if (choice == 2) {
+    if (computerChoice == 2) {
         console.log("Paper")
     }
-    if (choice == 3) {
+    if (computerChoice == 3) {
         console.log("Scissor")
     }
 }
