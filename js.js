f6101d052f8ed419389915dc9ffc42ec93fe2d84
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-07-24 20:29:50 +0200

    refactor: separate number-to-choice conversion logic

--- js.js
+++ js.js
@@ -1,14 +1,12 @@
+function convertNumberToChoice(choiceNumber) {
+    if (choiceNumber == 1) { return "Rock"; }
+    else if (choiceNumber == 2) { return "Paper"; }
+    else if (choiceNumber == 3) { return "Scissors"; }
+}
+
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3 + 1);
-    if (computerChoice == 1) {
-        console.log("Rock")
-    }
-    if (computerChoice == 2) {
-        console.log("Paper")
-    }
-    if (computerChoice == 3) {
-        console.log("Scissor")
-    }
+    let computerChoice = toString(Math.floor(Math.random() * 3 + 1));
+    return convertNumberToChoice(computerChoice)
 }
 
 getComputerChoice();
