2cd6f24d35146a23b8adbc51716077cd1c6c3d62
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-07-25 08:13:41 +0200

    feat: add round evaluation

--- js.js
+++ js.js
@@ -10,6 +10,24 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose rock [1], paper [2] or scissor [3].");
+    let humanChoice = prompt("Choose rock [1], paper [2] or scissors [3].");
     return convertNumberToChoice(humanChoice);
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "Rock") {
+        if (computerChoice == "Rock") { return "draw" }
+        if (computerChoice == "Paper") { return "lose" }
+        if (computerChoice == "Scissors") { return "win" }
+    }
+    if (humanChoice == "Paper") {
+        if (computerChoice == "Rock") { return "win" }
+        if (computerChoice == "Paper") { return "draw" }
+        if (computerChoice == "Scissors") { return "lose" }
+    }
+    if (humanChoice == "Scissors") {
+        if (computerChoice == "Rock") { return "lose" }
+        if (computerChoice == "Paper") { return "win" }
+        if (computerChoice == "Scissors") { return "draw" }
+    }
 }
